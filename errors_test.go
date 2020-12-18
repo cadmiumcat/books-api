a31deeb129d5a4bca5d02ff8904fce524e90fc23
Catalina <catalina.garcia.mail@gmail.com>
2020-12-18 13:03:54 +0000

    Test error for no name on checkout

--- errors_test.go
+++ errors_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 )
 
-
 func TestErrorMessage(t *testing.T) {
 	Convey("Given a checked out book", t, func() {
 		book := Book{
@@ -16,10 +15,21 @@ func TestErrorMessage(t *testing.T) {
 			user := "otherUser"
 			err := checkout(&book, user)
 
-			Convey("Then an error shows that the book is already checked out", func() {
+			Convey("Then an error message shows that the book is already checked out", func() {
 				So(err, ShouldBeError, "this book is currently checked out")
 			})
 		})
 
 	})
+
+	Convey("Given a book in the book store", t, func() {
+		book := Book{}
+		Convey("When a user tries to check out a book without providing a name", func() {
+			err := checkout(&book, "")
+			Convey("Then an error message shows that a name must be provided", func() {
+				So(err, ShouldBeError, "a name must be provided for checkout")
+			})
+		})
+	})
+
 }
