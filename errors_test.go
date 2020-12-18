dc907b6e6c383aac3762c6f76d6f9c470a109433
Catalina <catalina.garcia.mail@gmail.com>
2020-12-18 13:20:14 +0000

    Adds errors tests for checkin/out

--- errors_test.go
+++ errors_test.go
@@ -10,7 +10,6 @@ func TestErrorMessage(t *testing.T) {
 		book := Book{
 			History: []Checkout{{Who: "user"}},
 		}
-
 		Convey("When a User tries checks out the book", func() {
 			user := "otherUser"
 			err := checkout(&book, user)
@@ -20,9 +19,16 @@ func TestErrorMessage(t *testing.T) {
 			})
 		})
 
+		Convey("When a user tries to check in a book without providing a valid review", func() {
+			err := checkin(&book, 100)
+			Convey("Then an error message shows that a review must be provided", func() {
+				So(err, ShouldBeError, "a review between 1 and 5 must be provided")
+			})
+		})
+
 	})
 
-	Convey("Given a book in the book store", t, func() {
+	Convey("Given a book in the book store that has never been checked out", t, func() {
 		book := Book{}
 		Convey("When a user tries to check out a book without providing a name", func() {
 			err := checkout(&book, "")
@@ -30,6 +36,14 @@ func TestErrorMessage(t *testing.T) {
 				So(err, ShouldBeError, "a name must be provided for checkout")
 			})
 		})
+
+		Convey("When a user tries to check in the book", func() {
+			err := checkin(&book, 4)
+			Convey("Then an error message shows that the book has not been checked out", func() {
+				So(err, ShouldBeError, "this book is not currently checked out")
+			})
+		})
+
 	})
 
 }
