f3da74a811fd05d5d53c37502a866ffac14f02bb
Catalina <catalina.garcia.mail@gmail.com>
2021-02-02 14:30:37 +0000

    Adds omitempty bson to option field tags

--- models/books.go
+++ models/books.go
@@ -10,9 +10,9 @@ type Book struct {
 	ID       string     `json:"id" bson:"_id"`
 	Title    string     `json:"title" bson:"title"`
 	Author   string     `json:"author" bson:"author"`
-	Synopsis string     `json:"synopsis,omitempty" bson:"synopsis"`
-	Links    *Link      `json:"links,omitempty" bson:"links"`
-	History  []Checkout `json:"history,omitempty" bson:"history"`
+	Synopsis string     `json:"synopsis,omitempty" bson:"synopsis,omitempty"`
+	Links    *Link      `json:"links,omitempty" bson:"links,omitempty"`
+	History  []Checkout `json:"history,omitempty" bson:"history,omitempty"`
 }
 
 // Validate checks a Book for missing required fields.
@@ -43,6 +43,6 @@ type Link struct {
 
 // Books contains all the items (Book) in the library and a total count of those items
 type Books struct {
-	Count int    `json:"totalCount" bson:"totalCount"`
-	Items []Book `json:"items" bson:"items"`
+	Count int    `json:"totalCount"`
+	Items []Book `json:"items"`
 }
