6f0da16d0efcf62ba158138d9ef0ff85e40d4134
Catalina <catalina.garcia.mail@gmail.com>
2021-03-22 17:01:24 +0000

    Adds test for missing book review

--- api/reviews_test.go
+++ api/reviews_test.go
@@ -20,7 +20,7 @@ func TestReviews(t *testing.T) {
 		reviewID := "123"
 		mockDataStore := &datastoretest.DataStoreMock{
 			GetReviewFunc: func(ctx context.Context, id string) (*models.Review, error) {
-				return &models.Review{ID: "123"}, nil
+				return &models.Review{ID: reviewID}, nil
 			},
 		}
 
@@ -36,6 +36,34 @@ func TestReviews(t *testing.T) {
 			api.router.ServeHTTP(response, request)
 			Convey("Then the HTTP response code is 200", func() {
 				So(response.Code, ShouldEqual, http.StatusOK)
+			})
+			Convey("And the GetReview function is called once", func() {
+				So(mockDataStore.GetReviewCalls(), ShouldHaveLength, 1)
+			})
+		})
+	})
+
+	Convey("Given a existing book with no reviews", t, func() {
+		bookID := "1"
+		reviewID := "123"
+		mockDataStore := &datastoretest.DataStoreMock{
+			GetReviewFunc: func(ctx context.Context, id string) (*models.Review, error) {
+				return nil, ErrReviewMissing
+			},
+		}
+
+		ctx := context.Background()
+
+		api := Setup(ctx, host, mux.NewRouter(), mockDataStore, &hcMock)
+		Convey("When I send an HTTP GET request to /books/1/reviews/123", func() {
+			response := httptest.NewRecorder()
+
+			request, err := http.NewRequest(http.MethodGet, "/books/"+bookID+"/reviews/"+reviewID, nil)
+			So(err, ShouldBeNil)
+
+			api.router.ServeHTTP(response, request)
+			Convey("Then the HTTP response code is 404", func() {
+				So(response.Code, ShouldEqual, http.StatusNotFound)
 			})
 			Convey("And the GetReview function is called once", func() {
 				So(mockDataStore.GetReviewCalls(), ShouldHaveLength, 1)
