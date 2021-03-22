6860e6f11f5ef1016e1e61efa54262b875b45732
Catalina <catalina.garcia.mail@gmail.com>
2021-03-22 17:40:42 +0000

    Adds link struct to review

--- models/review.go
+++ models/review.go
@@ -1,11 +1,19 @@
 package models
 
+// A Review contains the fields that identify a review
 type Review struct {
-	ID string `json:"id" bson:"_id"`
+	ID    string `json:"id" bson:"_id"`
+	Links *link  `json:"links,omitempty" bson:"links,omitempty"`
+}
+
+// link is the relationship between a Book and a Review
+type link struct {
+	Self string
+	Book string
 }
 
 // Reviews contains all the items (Review) in the library and a total count of those items
 type Reviews struct {
-	Count int    `json:"totalCount"`
+	Count int      `json:"totalCount"`
 	Items []Review `json:"items"`
 }
